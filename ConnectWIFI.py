2f3388c99aad6789c06b87b5901bf23a427c76e8
SebbK <32422149+SebbK@users.noreply.github.com>
2018-05-02 00:37:49 +0200

    Delete ConnectWIFI.py

--- ConnectWIFI.py
+++
@@ -1,7 +0,0 @@
-#To connect to WiFi, - Sebastian Kleivenes
-from network import WLAN #Imports
-wlan=WLAN(mode=WLAN.STA)
-SSID=input("\nThis is for connecting to WiFi using WPA2 authentication\nWhat's the SSID of the network you are trying to connect to? ")
-WP=input("What's the password? ")
-wlan.connect(SSID, auth=(WLAN.WPA2,WP), timeout=5000)
-WLAN().ifconfig() #Provides local IP
